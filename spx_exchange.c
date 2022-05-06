999c0734ae6d1d51b7e5133166e9b38e97ff369a
anat9934 <anat9934@uni.sydney.edu.au>
2022-05-06 22:15:56 +1000

    wip

--- spx_exchange.c
+++ spx_exchange.c
@@ -20,10 +20,21 @@ char **trader_fifo;
 char received_msg[FIFO_LIMIT];
 char *sent_msg;
 char rec_order[ORDER_SIZE_LIMIT];
+pid_t children[FIFO_LIMIT];
+
+char *read_from_trader(int trader_id);
 
 void sig_handle(int sig) {
+    // get message/order
+    // READ till ;
+    // Parse input and print it (logic happens here)
     if (sig == SIGUSR1) {
         printf("caught-exchange\n");
+        for (int i = 0; i < num_products; i++) {
+            if (strcmp(read_from_trader(i), "BUY 0 GPU 30 500") == 0) {
+                printf("caught-exchange\n");
+            }
+        }
     }
 }
 
@@ -34,6 +45,11 @@ char *read_from_trader(int trader_id) {
     return get_message(received_msg);
 }
 
+void write_to_trader(int trader_id, char *message) {
+    write(exchange_fd[trader_id], message, strlen(message) + 1);
+    kill(children[trader_id], SIGUSR1);
+}
+
 int main(int argc, char **argv) {
     SPX_print("Starting\n");
     read_products(argv[1], &num_products, &products);
@@ -42,7 +58,7 @@ int main(int argc, char **argv) {
     for (int i = 0; i < num_products; ++i) {
         printf(" %s", products[i]);
     }
-	printf("\n");
+    printf("\n");
 
     // FIFO: Begin
     int num_traders = argc - 2;
@@ -51,8 +67,6 @@ int main(int argc, char **argv) {
     exchange_fifo = malloc(sizeof(char *) * num_traders);
     trader_fifo = malloc(sizeof(char *) * num_traders);
 
-    // pid_t children[FIFO_LIMIT];  // add children to this array
-
     for (int i = 0; i < num_traders; ++i) {
         exchange_fifo[i] = malloc(sizeof(char) * FIFO_LIMIT);
         trader_fifo[i] = malloc(sizeof(char) * FIFO_LIMIT);
@@ -60,53 +74,49 @@ int main(int argc, char **argv) {
         sprintf(trader_fifo[i], FIFO_TRADER, i);
 
         // Make FIFOs
-        mkfifo(exchange_fifo[i], 0666);
         SPX_print("Created FIFO /tmp/spx_exchange_%d\n", i);
+        mkfifo(exchange_fifo[i], 0666);
 
-        mkfifo(trader_fifo[i], 0666);
         SPX_print("Created FIFO /tmp/spx_trader_%d\n", i);
+        mkfifo(trader_fifo[i], 0666);
 
+        SPX_print("Starting trader %d (%s)\n", i, argv[2 + i]);
+
+        // Start each child process
         pid_t res = fork();
-        // pid_t cur_child = getpid();
         if (res == 0) {
             // child process
-			SPX_print("Starting trader %d (%s)\n", i, argv[2 + i]);
             char trader_id[FIFO_LIMIT];
             sprintf(trader_id, "%d", i);
             char *args[] = {argv[2 + i], trader_id, NULL};
 
-            // printf("[CHILD]: Trader is %d. Child is %d. Parent is %d.\n", getpid(), children[i], getppid());
+            // printf("[CHILD]: Exchange is %d. Child is %d. Parent is %d.\n", getpid(), res, getppid());
             execvp(argv[2 + i], args);
             exit(0);
         } else {
             // parent process
-            //  Send "CONNECT" to trader (child process)
+            signal(SIGUSR1, sig_handle);
+            children[i] = res;  // stores the process ID of each trader
 
+            // printf("[PARENT]: Trader is %d. Parent is %d. VSCODE is %d.\n", getpid(), res, getppid());
             SPX_print("Connected to /tmp/spx_exchange_%d\n", i);
-            SPX_print("Connected to /tmp/spx_trader_%d\n", i);
-            // printf("[PARENT]: Trader is %d. Child is %d. Parent is %d.\n", getpid(), children[i], getppid());
-
-			kill(res, SIGUSR1);
-
             exchange_fd[i] = open(exchange_fifo[i], O_WRONLY);
-            sent_msg = "MARKET OPEN;";
-            write(exchange_fd[i], sent_msg, strlen(sent_msg) + 1);
-            close(exchange_fd[i]);
-            // Wait for signal
-			signal(SIGUSR1, sig_handle);
-			pause();
 
-            if (strcmp(read_from_trader(i), "BUY 0 GPU 30 500") == 0) {
-                strcpy(rec_order, read_from_trader(i));
-                // rec_order = get_order(trade, &num_orders);
-                SPX_print("[T%d] Parsing command: <%s>\n", i, read_from_trader(i));
-            }
-
+            SPX_print("Connected to /tmp/spx_trader_%d\n", i);
+            trader_fd[i] = open(trader_fifo[i], O_RDONLY);
         }
     }
 
+    for (int i = 0; i < num_traders; i++) {
+        write_to_trader(i, "MARKET OPEN;");
+        kill(children[i], SIGUSR1);
+        // sent_msg = "MARKET OPEN;";
+        // write(exchange_fd[i], sent_msg, strlen(sent_msg) + 1);     
+    }
 
-    sleep(1);
+    // While loop checking how many SIGCHLDs signals received, if == num_traders -> calc fees
+    // If any SIGCHLD received --> disconnect the child and trader that sent it
+
     // Free products memory
     for (int i = 0; i < num_products; i++) {
         free(products[i]);
@@ -115,8 +125,13 @@ int main(int argc, char **argv) {
     free(products);
 
     for (int i = 0; i < num_traders; i++) {
+        // Free memory for FIFOs
         free(exchange_fifo[i]);
         free(trader_fifo[i]);
+
+        // Close all fd
+        close(exchange_fd[i]);
+        close(trader_fd[i]);
     }
     free(exchange_fd);
     free(trader_fd);
@@ -149,89 +164,4 @@ int read_products(char *path, int *num_products, char ***products) {
 
     fclose(file);
     return 0;
-}
-
-int SPX_print(const char *restrict format, ...) {
-    int res1, res2;
-    va_list ap;
-    res1 = printf("[SPX] ");
-    if (res1 < 0)
-        return res1;
-
-    va_start(ap, format);
-    res2 = vprintf(format, ap);
-    va_end(ap);
-    return res2 < 0 ? res2 : res1 + res2;
-}
-
-char *get_message(char *input) {
-    int delimiter;
-    for (int i = 0; i < FIFO_LIMIT; ++i)
-        if (input[i] == ';') {
-            delimiter = i;
-            break;
-        }
-
-    input[delimiter] = '\0';
-    return input;
-}
-
-int number_orders(char *line) {
-    int len_line = strlen(line);
-    int num_orders = 0;
-
-    char is_last_char_space = 1;
-    
-    // for each word
-    for (int line_i = 0; line_i < len_line - 1; line_i++) {
-        if (line[line_i] == ';')
-            is_last_char_space = 1;
-        else {
-            if (is_last_char_space)
-                num_orders++;
-
-            is_last_char_space = 0;
-        }
-    }
-    return num_orders;
-}
-
-char **get_order(char *line, int *num_orders) {
-    int len_line = strlen(line);
-    if (len_line == 0) {
-        return NULL;
-    }
-
-    *num_orders = number_orders(line);
-    char **words = (char **)malloc(*num_orders* sizeof(char *));
-    char is_last_char_space = 1;
-
-    int word_i = 0;
-    int first = 0;
-    int last = 0;
-
-    // for each word
-    for (int line_i = 0; line_i < len_line; line_i++) {
-        if (line[line_i] == ';') {
-            if (!is_last_char_space) {
-                last = line_i - 1;
-                words[word_i] = (char *)malloc((last - first + 2) * sizeof(char));
-                for (int i = first, j = 0; i <= last; i++, j++)
-                    words[word_i][j] = line[i];
-
-                words[word_i][last - first + 1] = '\0';
-                word_i++;
-            }
-
-            is_last_char_space = 1;
-        } else {
-            if (is_last_char_space) {
-                first = line_i;
-            }
-
-            is_last_char_space = 0;
-        }
-    }
-
-    return words;
 }

e5b42ca3c4d02c8d7b9d9db46536f19e02355244
anat9934 <anat9934@uni.sydney.edu.au>
2022-05-21 18:39:44 +1000

    dfg

--- tests/unit-tests.c
+++ tests/unit-tests.c
@@ -1,27 +1,29 @@
-// #include <setjmp.h>
+#include "../spx_exchange.h"
+#include "../spx_common.h"
+#include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
 
-#include "../spx_common.h"
 #include "cmocka.h"
 
 #define TESTING
-#include "../spx_exchange.c"
-#include "../spx_exchange.h"
+// #include "../spx_exchange.c"    
 #include "../spx_trader.h"
-
-static void test_get_PID(void **state) {
-    int PID = 1;
-    int result = get_PID(PID);
-    assert_int_equal(result, 0);
-}
 
 int main(void) {
     const struct CMUnitTest tests [] = {
         cmocka_unit_test(test_get_PID)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
+}
+
+
+
+static void test_get_PID(void **state) {
+    int PID = 1;
+    int result = get_PID(PID);
+    assert_int_equal(result, 0);
 }
